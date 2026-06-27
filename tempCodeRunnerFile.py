54c5b9d3944bd28e3d09b16b513e74a313f014f3
Your Name <huziiqbal00@gmail.com>
2026-06-27 23:30:33 +0530

    Turtle Graphics

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,75 @@
+from turtle import Turtle, Screen
+# huzi = Turtle()
+# huzi.shape("turtle")
+# huzi.color("pale violet red","firebrick")
+# huzi.pensize(5)
+# huzi.speed(4)
+# huzi.penup()
+# huzi.goto(-300, 0)
+# huzi.pendown()
+
+# def right_run():
+#     huzi.forward(100)
+#     huzi.right(90)
+
+
+# def move(dis):
+#     huzi.penup()
+#     huzi.forward(dis)
+#     huzi.pendown()
+
+
+# def down(dis):
+#     huzi.right(90)
+#     huzi.forward(dis)
+
+
+# def up(distance):
+#     huzi.backward(distance)
+
+
+# huzi.right(90)
+# huzi.forward(150)
+# huzi.penup()
+# huzi.backward(50)
+# huzi.pendown()
+# huzi.left(90)
+# right_run()
+# huzi.forward(50)
+# huzi.backward(150)
+# huzi.forward(150)
+
+# huzi.penup()
+# huzi.left(135)
+# huzi.forward(100)
+# huzi.pendown()
+# huzi.right(135)
+# huzi.forward(70)
+# huzi.left(90)
+# huzi.forward(50)
+# huzi.left(90)
+# huzi.forward(70)
+# huzi.penup()
+# huzi.right(90)
+# huzi.forward(30)
+# huzi.pendown()
+# huzi.forward(100)
+# huzi.right(135)
+# huzi.forward(100)
+# huzi.left(135)
+# huzi.forward(80)
+# huzi.penup()
+# huzi.forward(30)
+# huzi.left(90)
+# huzi.pendown()
+# huzi.forward(80)
+# huzi.penup()
+# huzi.forward(30)
+# huzi.dot(20)
+# huzi.penup()
+# huzi.forward(30)
+
+
+
+# screen = Screen()
+# screen.exitonclick()
