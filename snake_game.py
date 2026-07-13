4ccbc923edafbd38d207c8d247dfb395a3494f38
Your Name <huziiqbal00@gmail.com>
2026-07-13 20:57:09 +0530

    Snake game third draft

--- snake_game.py
+++ snake_game.py
@@ -1,16 +1,20 @@
-from turtle import Screen
+from turtle import Turtle , Screen
 from snake import Snake
 from Snake_Game_Food import Food
 from Snake_Game_Score import Score
 
 import time
+
+pencil = Turtle()
 screen = Screen()
+screen.setup(width=800, height=800)
 screen.bgcolor("black")
 screen.tracer(0)
 snake = Snake()
 food = Food()
 
 score = Score()
+
 
 
 screen.listen()
@@ -28,7 +32,11 @@ while game:
     if snake.snake[0].distance(food) < 15:
         food.collision()
         score.scoreboard()
-
+    if snake.snake[0].xcor() < -380 or snake.snake[0].xcor() > 380 or snake.snake[0].ycor() < -380 or snake.snake[0].ycor() > 380:
+        pencil.goto(0,0)
+        pencil.color("white")
+        pencil.write(f"Game Over")
+        break
 
 
 
