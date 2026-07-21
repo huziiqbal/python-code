296d315dfcc9db82d2a5434a8ab3eada035252e0
Your Name <huziiqbal00@gmail.com>
2026-07-21 21:58:16 +0530

    Added more bugs to fix later

--- Pong_game_ball.py
+++ Pong_game_ball.py
@@ -1,27 +1,50 @@
 from turtle import Turtle
 import random
-import time
 
 class Ball(Turtle):
     def __init__(self):
         super().__init__()
-        self.shape ("circle")
-        self.color ("white")
+        self.shape("circle")
+        self.color("white")
         self.penup()
 
     def one_cycle(self):
-        random1 = random.randint(-250,250)
-        random2 = random.randint(-250,250)
-        self.goto(0,-250)
-        time.sleep(0.5)
-        self.goto(350,random1)
-        time.sleep(0.5)
-        self.goto(0,-250)
-        time.sleep(0.5)
-        self.goto(-350,random2)
-        time.sleep(0.5)
-        self.goto(0,-250)
-        time.sleep(0.5)
+        cyc = random.randint(1,2)
+        if cyc == 1 :
+            randomx = random.randint(0,350)
+            randomy = random.randint(0,250)
+            x = randomx + x
+            y = randomy + y
+            while ( x < 350 or y < 250):
+                    self.goto(x , y)
+                    self.goto(0 , -250)
+
+        if cyc == 2 :
+            randomx = random.randint(0,350)
+            randomy = random.randint(0,-250)
+            x = randomx + x
+            y = randomy - y
+            while ( x < 350 or y < 250):
+                    self.goto(x , y)
+                    self.goto(0 , -250)
+
+
+
+
+
+
+
+
+        # self.goto(0,-250)
+        # time.sleep(0.01)
+        # self.goto(350,random1)
+        # time.sleep(0.01)
+        # self.goto(0,-250)
+        # time.sleep(0.01)
+        # self.goto(-350,random2)
+        # time.sleep(0.01)
+        # self.goto(0,-250)
+        # time.sleep(0.01)
 
 
 
